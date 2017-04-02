cd7b84e2de63f47f5d7a8c6f770c065402be1b2b
Sai-Kranti <p.saikranti@gmail.com>
2017-04-02 09:27:25 +0530

    safe

--- app/src/main/java/com/example/android/monday/InfoActivity.java
+++ app/src/main/java/com/example/android/monday/InfoActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.monday;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +16,24 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setText("Startup");
+        tabLayout.getTabAt(1).setText("Advantages");
+        tabLayout.getTabAt(2).setText("Disadvantages");
+
     }
 
     public void click_ft(View view)
@@ -23,6 +46,33 @@ public class InfoActivity extends AppCompatActivity {
     {
         Intent ii = new Intent(this, TailorActivity.class);
         startActivity(ii);
+    }
+
+
+
+
+    public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public SimpleFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new StartupFragment();
+            } else if (position == 1){
+                return new AdvantagesFragment();
+            } else {
+                return new DisadvantagesFragment();
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/android/monday/StartupFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.monday;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class StartupFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_startup, container, false);
+    }
+
+
+
+}
